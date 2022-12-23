761378910007cf86282b1878e3ce146c929c89d0
Eugene <toropov_evgenii@mail.ru>
2022-12-24 00:07:43 +0300

    comments and docstrings added

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template, jsonify  # Импортируем необходимые библиотеки и модули
 import utils
 import logging
 
+# Создаем логгер, устанавливаем уровень INFO, задаем обработчик, указываем файл для записей,
+# Указываем кодировку, указываем формат записи.
 logger = logging.getLogger('logger')
 logger.setLevel(logging.INFO)
 logger_handler = logging.FileHandler('logs/api.log', encoding='utf-8')
@@ -10,18 +12,28 @@ logger_formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
 logger_handler.setFormatter(logger_formatter)
 logger.addHandler(logger_handler)
 
-app = Flask(__name__)
-app.config['JSON_AS_ASCII'] = False
+
+app = Flask(__name__)  # Регистрируем приложение
+app.config['JSON_AS_ASCII'] = False  # Указываем параметр конфигурации для кодировки
 
 
 @app.route('/')
 def main_page():
+    """Представление главной страницы по маршруту '/'.
+
+    Внутри представления обращаемся к функции, которая
+    возвращает список словарей. Полученные данные передаем в шаблон index.html"""
     items = utils.get_posts_all()
     return render_template("index.html", items=items)
 
 
 @app.route('/posts/<int:post_pk>')
 def post_page(post_pk):
+    """Представление по маршруту '/posts/<int:post_pk>'
+
+    Представление обращается к функции с аргументом, которая
+    возвращает список. Создаем переменные и присваиваем им значения из
+    полученных данных, далее все переменные передаем в шаблон 'post.html'"""
 
     post = utils.get_post_by_pk(post_pk)
     poster_name = post["poster_name"]
@@ -47,6 +59,12 @@ def post_page(post_pk):
 
 @app.route('/search')
 def search_word():
+    """Представление по маршруту /search
+
+    Из запроса берем строку и присваиваем ее к переменной substr.
+    Обращаемся к функции с аргументом substr, получаем список словарей.
+    Ограничиваем его размер до 10. Задаем необходимые переменные и
+    передаем в шаблон 'search.html'"""
     substr = request.args.get('s', '')
     data = utils.search_for_posts(substr)
     posts = data[:10]
@@ -56,6 +74,11 @@ def search_word():
 
 @app.route('/users/<user_name>')
 def search_name(user_name):
+    """Представление '/users/<user_name>'
+
+    При помощи конструкции try/except обращаемся к функции с аргументом: <user_name>.
+    Получаем список, задаем переменные и передаем данные в шаблон 'user-feed.html'.
+    Если пользователя не окажется в списке - возвращаем соответствующее сообщение"""
     try:
         posts = utils.get_posts_by_user(user_name)
         username = user_name
@@ -66,6 +89,10 @@ def search_name(user_name):
 
 @app.route('/api/posts')
 def api_all_posts():
+    """API представление по маршруту '/api/posts'.
+
+    Обращаемся к функции, получаем список словарей, используем функцию jsonify и
+    возвращаем данные. При каждом обращении к маршруту вносим соответствующую запись в лог"""
     logger.info("Запрос /api/posts")
     data = utils.get_posts_from_json()
     return jsonify(data)
@@ -73,6 +100,10 @@ def api_all_posts():
 
 @app.route('/api/posts/<int:post_pk>')
 def api_post(post_pk):
+    """API представление по маршруту '/api/posts/<post_pk>'.
+
+    Обращаемся к функции, получаем словарь, используем функцию jsonify и
+    возвращаем данные. При каждом обращении к маршруту вносим соответствующую запись в лог"""
     logger.info(f"Запрос /api/posts/{post_pk}")
     data = utils.get_post_by_pk(post_pk)
     return jsonify(data)
@@ -80,13 +111,19 @@ def api_post(post_pk):
 
 @app.errorhandler(404)
 def page_not_found(e):
+    """Обработчик запросов к несуществующим страницам.
+
+    Возвращает ошибку и статус-код 404"""
     return e, 404
 
 
 @app.errorhandler(500)
 def page_not_found(e):
+    """Обработчик ошибок со стороны сервера.
+
+    Возвращает ошибку и статус-код 500"""
     return e, 500
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # Условия для запуска приложения
     app.run()
--- utils.py
+++ utils.py
@@ -1,8 +1,13 @@
-from json import JSONDecodeError
+from json import JSONDecodeError  # Импортируем необходимые библиотеки и модули
 import json
 
 
 def get_posts_from_json():
+    """Функция загрузки данных из файла.
+
+    Используя конструкцию try/except при помощи оператора with open открываем JSON файл.
+    Вызываем исключения если файл не найден или его не удается преобразовать.
+    Возвращаем список"""
     try:
         with open("data/posts.json", "r", encoding="utf-8") as file:
             data = json.load(file)
@@ -14,19 +19,26 @@ def get_posts_from_json():
 
 
 def get_posts_all():
+    """Функция возвращения данных с обращением к предыдущей функции.
+
+    Возвращает список словарей"""
     return get_posts_from_json()
 
 
 def get_posts_by_user(user_name):
+    """Функция поиска постов по имени пользователя
 
-    if user_name == None:
-        raise TypeError("Аргумент не найден")
+    Проверяем тип аргумента и наличие его в списке имен пользователей,
+    в случае провальной проверки вызываем исключение с соответствующим сообщением.
+    Обращаемся к функции get_posts_all() и при помощи цикла for проходим по полученным данным,
+    сравнивая аргумент user_name со значением по ключу "poster_name". При соответствии добавляем
+    данные о посте в пустой список и возвращаем список"""
 
     if type(user_name) != str:
         raise TypeError("Аргумент должен быть строкой")
 
     poster_names = []
-    for post in get_posts_from_json():
+    for post in get_posts_all():
         poster_names.append(post['poster_name'].lower())
 
     if user_name.lower() not in poster_names:
@@ -40,6 +52,11 @@ def get_posts_by_user(user_name):
 
 
 def get_comments_from_json():
+    """Функция загрузки комментариев из JSON файла.
+
+    Используя конструкцию try/except при помощи оператора with open открываем JSON файл.
+    Вызываем исключения если файл не найден или его не удается преобразовать.
+    Возвращаем список"""
     try:
         with open("data/comments.json", "r", encoding="utf-8") as file:
             data = json.load(file)
@@ -51,12 +68,16 @@ def get_comments_from_json():
 
 
 def get_comments_by_post_id(post_id):
+    """Функция поиска комментариев по аргументу post_id
+
+    Проверяем тип аргумента и наличие в списке существующих id постов,
+    в случаем провальной проверки вызываем исключения и возвращаем соответствующие сообщения.
+    Обращаемся к функции get_comments_from_json() и при помощи цикла for проходим по полученным данным,
+    сравнивая аргумент post_id со значением по ключу "post_id". При соответствии добавляем данные о комментарии
+    в пустой список и возвращаем список"""
 
     if post_id <= 0:
         raise ValueError("Аргумент должен быть числом больше 0")
-
-    if post_id == None:
-        raise TypeError("Аргумент не найден")
 
     if type(post_id) != int or type(post_id) == float:
         raise TypeError("Аргумент должен быть целым числом")
@@ -76,12 +97,15 @@ def get_comments_by_post_id(post_id):
 
 
 def search_for_posts(word):
+    """Функция поиска поста по аргументу word.
+
+    Проверяем тип аргумента, при провальной проверки вызываем исключение и возвращаем соответствующее сообщение.
+    Обращаемся к функции get_posts_from_json() и при помощи цикла for проходим по полученным данным,
+    проверяя наличие аргумента word в значении по ключу 'content'. При соответствии добавляем данные о посте
+    в пустой список и возвращаем список"""
 
     if type(word) != str:
         raise TypeError("Аргумент должен быть строкой")
-
-    if word == None:
-        raise TypeError("Аргумент не найден")
 
     posts_with_word = []
     for post in get_posts_from_json():
@@ -91,12 +115,16 @@ def search_for_posts(word):
 
 
 def get_post_by_pk(pk):
+    """Функция поиска поста по аргументу pk.
+
+    Проверяем тип аргумента и его значение, в случаем провальной проверки вызываем исключения
+    и возвращаем соответствующие сообщения.
+    Обращаемся к функции get_posts_from_json() и при помощи цикла for проходим по полученным данным,
+    сравнивая аргумент pk со значением по ключу 'pk'. При соответствии возвращаем данные о посте
+    в виде словаря"""
 
     if pk <= 0:
         raise ValueError("Аргумент должен быть положительным числом больше 0")
-
-    if pk == None:
-        raise TypeError("Аргумент не найден")
 
     if type(pk) == str or type(pk) == float:
         raise TypeError("Аргумент должен быть целым числом")
@@ -104,5 +132,3 @@ def get_post_by_pk(pk):
     for post in get_posts_from_json():
         if post['pk'] == pk:
             return post
-
-
